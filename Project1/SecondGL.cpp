427aa28fc21b3f4d3426d922e6f9348f28b0852a
Conerlius <747630335@qq.com>
2019-08-29 10:34:02 +0800

    修正工程报错

--- Project1/SecondGL.cpp
+++ Project1/SecondGL.cpp
@@ -2,22 +2,24 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-void initGLFW1();
-int drawWindow1();
+void initGLFW2();
+int drawWindow2();
+void framebuffer_size_callback2(GLFWwindow* window, int width, int height);
+void processInput2(GLFWwindow* window);
 
 int main1()
 {
-	initGLFW1();
-	int result = drawWindow1();
+	initGLFW2();
+	int result = drawWindow2();
 	return result;
 }
-void initGLFW1() {
+void initGLFW2() {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
-int drawWindow1() {
+int drawWindow2() {
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL) {
 		glfwTerminate();
@@ -27,6 +29,19 @@ int drawWindow1() {
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		return 1;
 	}
-
+	//窗口大小被调整的回调
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback2);
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput2(window);
+	}
 	return 1;
+}
+
+void framebuffer_size_callback2(GLFWwindow* window, int width, int height) {
+	// 设置窗口的维度
+	glViewport(0, 0, width, height);
+}
+
+void processInput2(GLFWwindow* window) {
 }
