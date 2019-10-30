c460975b90222c0e8cd1a94619184af70074099d
Conerlius <747630335@qq.com>
2019-10-30 18:00:31 +0800

    修正课程2的报错

--- Project1/OpenGl_Tools.cpp
+++ Project1/OpenGl_Tools.cpp
@@ -1,5 +1,8 @@
 #include "OpenGl_Tools.h"
 #include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
 
 OpenGl_Tools::OpenGl_Tools()
 {
@@ -7,24 +10,29 @@ OpenGl_Tools::OpenGl_Tools()
 OpenGl_Tools::~OpenGl_Tools()
 {
 }
-
-GLuint OpenGl_Tools::GetShader(const char* name) 
+// 获取已经编译的shader
+GLuint OpenGl_Tools::GetShader(string name) 
 {
-	std::map<const char*, GLuint>::iterator it = shader_map.find(name);
+	map<string, GLuint>::iterator it = shader_map.find(name);
 	if (it != shader_map.end()) {
-		return *(it->first);
+		return it->second;
 	}
 	else
 	{
 		return -1;
 	}
+	return -1;
 }
-
-void OpenGl_Tools::CompileShader(const char* vertex_path, const char* fragment_path, const char* name)
+// 编译shader
+// vertex_path	vertex文件路径
+// fragment_path	fragment文件路径
+// name		shader名称
+void OpenGl_Tools::CompileShader(const char* vertex_path, const char* fragment_path, string name)
 {
+	// 声明Vertex和Fragment的对象id
 	GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
 	GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
-
+	// 打开Vertex文本
 	std::string VertexShaderCode;
 	std::ifstream VertexShaderStream (vertex_path, std::ios::in);
 	if (VertexShaderStream.is_open()) {
@@ -38,6 +46,7 @@ void OpenGl_Tools::CompileShader(const char* vertex_path, const char* fragment_p
 		getchar();
 		return ;
 	}
+	// 打开Fragment文本
 	std::string FragmentShaderCode;
 	std::ifstream FragmentShaderStream(fragment_path, std::ios::in);
 	if (FragmentShaderStream.is_open()) {
@@ -51,15 +60,16 @@ void OpenGl_Tools::CompileShader(const char* vertex_path, const char* fragment_p
 		getchar();
 		return ;
 	}
+	// 开始编译
 	GLint Result = GL_FALSE;
 	int InfoLogLength;
-
-	printf("Compiling shader : %s\n", vertex_file_path);
+	// Compile Vertex Shader
+	printf("Compiling shader : %s\n", vertex_path);
 	char const* VertexSourcePointer = VertexShaderCode.c_str();
 	glShaderSource(VertexShaderID, 1, &VertexSourcePointer, NULL);
 	glCompileShader(VertexShaderID);
 
-	// Check Vertex Shader
+	// 检查Vertex编译情况
 	glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
 	glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
 	if (InfoLogLength > 0) {
@@ -67,16 +77,13 @@ void OpenGl_Tools::CompileShader(const char* vertex_path, const char* fragment_p
 		glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
 		printf("%s\n", &VertexShaderErrorMessage[0]);
 	}
-
-
-
 	// Compile Fragment Shader
-	printf("Compiling shader : %s\n", fragment_file_path);
+	printf("Compiling shader : %s\n", fragment_path);
 	char const* FragmentSourcePointer = FragmentShaderCode.c_str();
 	glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer, NULL);
 	glCompileShader(FragmentShaderID);
 
-	// Check Fragment Shader
+	// 检查Fragment编译情况
 	glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
 	glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
 	if (InfoLogLength > 0) {
@@ -85,8 +92,6 @@ void OpenGl_Tools::CompileShader(const char* vertex_path, const char* fragment_p
 		printf("%s\n", &FragmentShaderErrorMessage[0]);
 	}
 
-
-
 	// Link the program
 	printf("Linking program\n");
 	GLuint ProgramID = glCreateProgram();
@@ -94,7 +99,7 @@ void OpenGl_Tools::CompileShader(const char* vertex_path, const char* fragment_p
 	glAttachShader(ProgramID, FragmentShaderID);
 	glLinkProgram(ProgramID);
 
-	// Check the program
+	// 检查 shader program的情况
 	glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
 	glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
 	if (InfoLogLength > 0) {
@@ -103,12 +108,11 @@ void OpenGl_Tools::CompileShader(const char* vertex_path, const char* fragment_p
 		printf("%s\n", &ProgramErrorMessage[0]);
 	}
 
-
 	glDetachShader(ProgramID, VertexShaderID);
 	glDetachShader(ProgramID, FragmentShaderID);
 
 	glDeleteShader(VertexShaderID);
 	glDeleteShader(FragmentShaderID);
-
-	shader_map.insert(std::pair<const char*, GLuint>(name, ProgramID));
+	// 记录
+	shader_map.insert(std::pair<string, GLuint>(name, ProgramID));
 }
--- Project1/OpenGl_Tools.h
+++ Project1/OpenGl_Tools.h
@@ -2,15 +2,25 @@
 #include <glad/glad.h>
 #include <iostream>
 #include <map>
+#include <string>
 
 class OpenGl_Tools {
 private:
-	static std::map<const char*, GLuint> shader_map;
+	std::map<std::string, GLuint> shader_map;
+	static OpenGl_Tools* m_pInstance;
 public:
 	OpenGl_Tools();
 	~OpenGl_Tools();
 	// 通过shader名称获取gpu上的shader程序对象
-	static GLuint GetShader(const char* name);
+	GLuint GetShader(std::string name);
 	// 编译shader
-	static void CompileShader(const char* vertex_path, const char* fragment_path, const char* name);
+	void CompileShader(const char* vertex_path, const char* fragment_path, std::string name);
+
+	static OpenGl_Tools* GetInstance()
+	{
+		if (m_pInstance == NULL)  //判断是否第一次调用
+			m_pInstance = new OpenGl_Tools();
+		return m_pInstance;
+	}
+
 };
